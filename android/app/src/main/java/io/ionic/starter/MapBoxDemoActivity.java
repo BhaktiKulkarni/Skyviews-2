8f5edf72e0676dc7ec6862c3eff86ec02bf619fd
Bhakti Kulkarni <bhaktik@WinjitTechnologiesPvtLtd.local>
2019-07-11 19:03:51 +0530

    Navigation in android

---
+++ android/app/src/main/java/io/ionic/starter/MapBoxDemoActivity.java
@@ -0,0 +1,13 @@
+package io.ionic.starter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapBoxDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_box_demo);
+    }
+}
