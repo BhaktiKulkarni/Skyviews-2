deed99d8aebbb5e7c4160324fc5ae0a53ef5e12c
Bhakti Kulkarni <bhaktik@WinjitTechnologiesPvtLtd.local>
2019-07-12 12:01:42 +0530

    Navigation in android

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,21 +1,38 @@
 package io.ionic.starter;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 
 import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-    }});
-  }
+public class MainActivity extends BridgeActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        // Initializes the Bridge
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+            // Additional plugins you've installed go here
+            // Ex: add(TotallyAwesomePlugin.class);
+        }});
+
+
+
+    }
 }
--- android/app/src/main/java/io/ionic/starter/MapBoxDemoActivity.java
+++ android/app/src/main/java/io/ionic/starter/MapBoxDemoActivity.java
@@ -2,12 +2,69 @@ package io.ionic.starter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MapBoxDemoActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private MapboxNavigation navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_box_demo);
+
+        Mapbox.getInstance(this, getResources().getString(R.string.access_token));
+
+        Toast.makeText(this, "Hello", Toast.LENGTH_LONG);
+
+        navigation = new MapboxNavigation(this, getResources().getString(R.string.access_token));
+
+        // From Mapbox to The White House
+        Point origin = Point.fromLngLat(-77.03613, 38.90992);
+        Point destination = Point.fromLngLat(-77.0365, 38.8977);
+
+        NavigationRoute.builder(this)
+                .accessToken(getResources().getString(R.string.access_token))
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        Log.d(TAG, "onResponse: " + response);
+                        Log.d(TAG, "onResponse: Call" + call);
+
+                        DirectionsResponse directionsRoute = response.body();
+
+                        NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                                .directionsRoute(directionsRoute.routes().get(0))
+                                .shouldSimulateRoute(false)
+                                .build();
+
+// Call this method with Context from within an Activity
+                        NavigationLauncher.startNavigation(MapBoxDemoActivity.this, options);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                        Log.d(TAG, "onFailure: " + call);
+                    }
+                });
+
+
     }
 }
